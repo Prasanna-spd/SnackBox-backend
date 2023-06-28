27daa13e6eceba03f47be0f96ba9c3236c6ed5cc
Sarada Prasanna Dash <dashsharada@gmail.com>
2023-06-28 07:01:09 +0530

    changed url and redirect uri

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.use(passport.session());
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", `${clientUrl}`);
+  // res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
--- passpor.js
+++ passpor.js
@@ -6,7 +6,7 @@ passport.use(
     {
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: "https://snack-box-back.onrender.com/auth/google/callback",
       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     function (accessToken, refreshToken, profile, done) {
